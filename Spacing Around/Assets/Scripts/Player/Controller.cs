daa99910bbb11555a7b7ea6f6e15dc878f93eba5
Skinke <mer785@hotmail.com>
2019-08-25 23:59:12 +0200

    Controls.. Movement..
    Doesn't feel right...
    Need more work...

--- Spacing Around/Assets/Scripts/Player/Controller.cs
+++ Spacing Around/Assets/Scripts/Player/Controller.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D myRB;
+
+    public float accSpeed;
+    public float maxSpeed;
+    public float maxRotationSpeed;
+    public float curSpeed;
+    Vector3 forwardSpeed, maximumSpeed, currentSpeed;
+
+    private void Start()
+    {
+        accSpeed = 30;
+        maxRotationSpeed = 200;
+        maxSpeed = 1000;
+        myRB = GetComponent<Rigidbody2D>();
+        maximumSpeed = maxSpeed * transform.up;
+        currentSpeed = accSpeed * transform.up;
+        print("MaximumSpeed: " + maximumSpeed.y);
+        print("CurrentSpeed: " + currentSpeed.y);
+    }
+    private void Update()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            forwardSpeed = accSpeed * transform.up;
+            if (forwardSpeed.y < maximumSpeed.y)
+            {
+                myRB.AddForce(forwardSpeed);
+                print("adding Speed..");
+            }
+            else
+            {
+                forwardSpeed = maximumSpeed;
+                print("Maximum Speed added..");
+            }
+          
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * maxRotationSpeed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * -maxRotationSpeed * Time.deltaTime, Space.Self);
+        }
+        curSpeed = myRB.velocity.magnitude;
+        //if(curSpeed > 0)
+        //    print(myRB.velocity.magnitude);
     }
 }
