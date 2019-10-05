e5aba8073b681b81e1a9e37bf34eb91dfd94daa7
Skinke <mer785@hotmail.com>
2019-10-05 21:11:35 +0200

    Beginning of Item_Class

---
+++ Spacing Around/Assets/Scripts/Item/BaseItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class IBaseItem
+{
+    public enum Rarity
+    {
+        Common, UnCommon, Rare, Epic, Legendary, Alien, Cosmic
+    }
+    public enum ItemType
+    {
+        Money, ShipPart, TechApp,
+    }
+
+    private string _BIName;
+    protected int _BIValue;
+    protected int _BIValueVendorCost;
+
+    protected Rarity _BIRarity;
+    protected ItemType _BITypeOfItem;
+
+
+}
---
+++ Spacing Around/Assets/Scripts/Item/Weapon/IWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Item.Weapon
+{
+    interface IWeapon
+    {
+        void SwitchWeapon();
+        void AttackMode();
+        void Shoot(int weaponDamage);
+    }
+}
