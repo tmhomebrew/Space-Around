ef4c15eb9220915329bfef9f36babc42f9e16d52
Skinke <mer785@hotmail.com>
2019-10-07 17:36:28 +0200

    WeaponLaser
    Change laserShot, based on WeaponItem_Rarity..

--- Spacing Around/Assets/Scripts/LaserShot.cs
+++ Spacing Around/Assets/Scripts/LaserShot.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LaserShot : MonoBehaviour
 {
+    WeaponLaser myWL;
+
     public enum LaserType
     {
         Green,
+        LightBlue,
         Blue,
         Yellow,
         Red,
         Purple
     }
 
+    [SerializeField]
+    public Sprite[] laserBeamSprite = new Sprite[5];
+    Sprite mySprite;
+    LaserType myLT;
     //Mangler reference til WeaponLaser........... Construct ? <-----
 
     [SerializeField]
@@ -22,27 +29,40 @@ public class LaserShot : MonoBehaviour
 
     Rigidbody2D myRB;
 
-    Vector3 forwardSpeed;
-
     SpriteRenderer mySprRend;
 
-    [SerializeField]
-    public Sprite[] laserBeamSprite = new Sprite[5];
-    Sprite mySprite;
 
     public GameObject LaserOwner { get => laserOwner; set => laserOwner = value; }
+
+    public LaserShot()
+    {
+        //switch (myWL.LaserRarity)
+        //{
+        //    case BaseItem.Rarity.Common:
+        //        myLT = LaserType.Blue;
+        //        mySprite = laserBeamSprite[1];
+        //        break;
+        //    default:
+        //        break;
+        //}
+        
+    }
 
     void Awake()
     {
         LaserOwner = transform.root.transform.GetComponentInChildren<ShipStats>().gameObject;
+        myWL = LaserOwner.GetComponentInChildren<WeaponLaser>();
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), LaserOwner.GetComponentInChildren<Collider2D>());
         myRB = GetComponent<Rigidbody2D>();
 
         mySprRend = GetComponent<SpriteRenderer>();
+        //SetStats(LaserType.Green);
+        //mySprRend.sprite = mySprite;  <---------------------------------------------
+        
+        
         //Destroy(GetComponent<PolygonCollider2D>()); //Refreshing the Collider
         //gameObject.AddComponent<PolygonCollider2D>(); //Refreshing the Collider
 
-        //SetStats(LaserType.Green);
     }
 
     // Start is called before the first frame update
@@ -84,33 +104,4 @@ public class LaserShot : MonoBehaviour
         yield return new WaitForSeconds(5f);
         Destroy(gameObject);
     }
-
-    //public void SetStats(LaserType LT)
-    //{
-    //    speed = 10;
-    //    switch (LT)
-    //    {
-    //        case LaserType.Green:
-    //            damage = 2;
-    //            mySprite = laserBeamSprite[0];
-    //            break;
-    //        case LaserType.Blue:
-    //            damage = 4;
-    //            mySprite = laserBeamSprite[1];
-    //            break;
-    //        case LaserType.Yellow:
-    //            mySprite = laserBeamSprite[2];
-    //            break;
-    //        case LaserType.Red:
-    //            mySprite = laserBeamSprite[3];
-    //            break;
-    //        case LaserType.Purple:
-    //            damage = 100;
-    //            mySprite = laserBeamSprite[4];
-    //            break;
-    //        default:
-    //            break;
-    //    }
-    //    mySprRend.sprite = mySprite;
-    //}
 }
