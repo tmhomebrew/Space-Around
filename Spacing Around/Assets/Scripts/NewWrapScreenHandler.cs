73770b56099ede5b4ce2174a1124e97e6422f6b7
THMATcaa <36071519+ThmaTech@users.noreply.github.com>
2019-10-18 09:01:22 +0200

    Comments
    Comments  added to WrapScreenHandler

--- Spacing Around/Assets/Scripts/NewWrapScreenHandler.cs
+++ Spacing Around/Assets/Scripts/NewWrapScreenHandler.cs
@@ -5,41 +5,52 @@ using UnityEngine;
 public class NewWrapScreenHandler : MonoBehaviour
 {
     BoxCollider2D myCol;
+    float boundingBoxSizeX, boundingBoxSizeY;
 
     private void Start()
     {
         myCol = GetComponent<BoxCollider2D>();
+        boundingBoxSizeX = myCol.size.x;
+        boundingBoxSizeY = myCol.size.y;
     }
 
+    /// <summary>
+    /// Wrap-methoed - Teleports an objects.transform to the other side of the map, depending on the point in worldSpace, where it leaves the boundingBoxArea.
+    /// </summary>
+    /// <param name="GO">transform of the Gameobject.</param>
     void Wrap(Transform GO)
     {
         //x-axis, Left side
-        if (GO.transform.position.x < (myCol.size.x / 2) * -1)
+        if (GO.transform.position.x < (boundingBoxSizeX / 2) * -1)
         {
-            GO.transform.position = new Vector3((myCol.size.x / 2) * 1, GO.transform.position.y, GO.transform.position.z);
+            GO.transform.position = new Vector3((boundingBoxSizeX / 2) * 1, GO.transform.position.y, GO.transform.position.z);
         }
         //x-axis, Right side
-        if (GO.transform.position.x > (myCol.size.x / 2) * 1)
+        if (GO.transform.position.x > (boundingBoxSizeX / 2) * 1)
         {
-            GO.transform.position = new Vector3((myCol.size.x / 2) * -1, GO.transform.position.y, GO.transform.position.z);
+            GO.transform.position = new Vector3((boundingBoxSizeX / 2) * -1, GO.transform.position.y, GO.transform.position.z);
         }
         //y-axis, Top side
-        if (GO.transform.position.y > (myCol.size.y / 2) * 1)
+        if (GO.transform.position.y > (boundingBoxSizeY / 2) * 1)
         {
-            GO.transform.position = new Vector3(GO.transform.position.x, (myCol.size.y / 2) * -1, GO.transform.position.z);
+            GO.transform.position = new Vector3(GO.transform.position.x, (boundingBoxSizeY / 2) * -1, GO.transform.position.z);
         }
         //y-axis, Right side
-        if (GO.transform.position.y < (myCol.size.y / 2) * -1)
+        if (GO.transform.position.y < (boundingBoxSizeY / 2) * -1)
         {
-            GO.transform.position = new Vector3(GO.transform.position.x, (myCol.size.y / 2) * 1, GO.transform.position.z);
+            GO.transform.position = new Vector3(GO.transform.position.x, (boundingBoxSizeY / 2) * 1, GO.transform.position.z);
         }
     }
 
+    /// <summary>
+    /// Registres if an object leaves the boundingBox of the Level.
+    /// </summary>
+    /// <param name="col">Object.Collider leaving the boundingBoxArea</param>
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.transform.root.name == "Player")
         {
-            Wrap(col.transform.root.GetComponentInChildren<ShipStats>().transform);
+            Wrap(col.transform.root.GetComponentInChildren<ShipStats>().transform); //PlayerPrefab-->SpaceShip.transform
             //print("Player has been moved.." + col.transform.root.GetComponentInChildren<ShipStats>().transform);
         }
         else
@@ -47,6 +58,5 @@ public class NewWrapScreenHandler : MonoBehaviour
             Wrap(col.transform);
             //print("Something entered the field");
         }
-
     }
 }
