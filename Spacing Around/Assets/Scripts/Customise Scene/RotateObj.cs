19ca75b69704700fad23d5b0771c0985c38940cd
THMATcaa <36071519+ThmaTech@users.noreply.github.com>
2020-10-28 14:19:40 +0100

    Update RotateObj.cs
    Scaling of objects
    - reset when left/right click

--- Spacing Around/Assets/Scripts/Customise Scene/RotateObj.cs
+++ Spacing Around/Assets/Scripts/Customise Scene/RotateObj.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class RotateObj : MonoBehaviour
 {
     [SerializeField]
-    bool isRunning; //For Inspector Purpose
+    bool isRotating, isZooming, canRotate, canZoom; //For Inspector Purpose
     public float RotationSpeed = 200f;
     Quaternion startRot;
+    Vector3 startScale;
 
     private void Start()
     {
-        isRunning = false;
+        isRotating = false;
+        isZooming = false;
+        canRotate = false;
+        canZoom = false;
+
         startRot = transform.localRotation;
+        startScale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -23,19 +29,39 @@ public class RotateObj : MonoBehaviour
 
     public void ClickedOnObject()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (canRotate)
         {
-            StopCoroutine(RotateTowardsStartingPoint());
-            isRunning = false;
+            if (Input.GetMouseButtonDown(0))
+            {
+                StopCoroutine(RotateTowardsStartingPoint());
+                isRotating = false;
+            }
+            if (Input.GetMouseButton(0))
+            {
+                transform.Rotate(
+                    (Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime),
+                    (Input.GetAxis("Mouse X") * -RotationSpeed * Time.deltaTime), 
+                    0, 
+                    Space.Self
+                );
+            }
         }
-        if (Input.GetMouseButton(0))
+        if (canZoom)
         {
-            transform.Rotate(
-                (Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime),
-                (Input.GetAxis("Mouse X") * -RotationSpeed * Time.deltaTime), 
-                0, 
-                Space.Self
-            );
+            if (Input.mouseScrollDelta.y != 0 && !isZooming)
+            {
+                print("Mouse Scroll Input - " + Input.mouseScrollDelta.y * 0.1f);
+                //Zoom in
+                if (Input.mouseScrollDelta.y > 0)
+                {
+                    StartCoroutine(ZoomInOnObject(0.1f, Vector3.one * 1.1f));
+                }
+                //Zoom out
+                if (Input.mouseScrollDelta.y < 0)
+                {
+                    StartCoroutine(ZoomInOnObject(0.1f, Vector3.one * -1.1f));
+                }
+            }
         }
         if (Input.GetMouseButtonUp(1))
         {
@@ -43,13 +69,17 @@ public class RotateObj : MonoBehaviour
             {
                 StartCoroutine(RotateTowardsStartingPoint());
             }
+            if (transform.localScale != startScale)
+            {
+                StartCoroutine(ZoomInOnObject(0.5f, startScale));
+            }
         }
     }
 
     IEnumerator RotateTowardsStartingPoint()
     {
-        isRunning = true;
-        while (transform.localRotation != startRot && isRunning == true)
+        isRotating = true;
+        while (transform.localRotation != startRot && isRotating == true)
         {
             transform.localRotation = Quaternion.RotateTowards(transform.localRotation, startRot, RotationSpeed * Time.deltaTime);
             yield return new WaitForSeconds(0.01f);
@@ -58,6 +88,48 @@ public class RotateObj : MonoBehaviour
                 break;
             }
         }
-        isRunning = false;
+        isRotating = false;
+    }
+
+    Vector3 originalScale, targetScale;
+
+    IEnumerator ZoomInOnObject(float time, Vector3 zoomScale)
+    {
+        isZooming = true;
+        if (zoomScale != startScale)
+        {
+            originalScale = transform.localScale;
+            targetScale = transform.localScale + zoomScale;
+        }
+        else
+        {
+            originalScale = transform.localScale;
+            targetScale = zoomScale;
+        }
+        float originalTime = time;
+
+        while (time > 0.0f)
+        {
+            time -= Time.deltaTime;
+
+            if (targetScale.y > 5 || targetScale.y < 1)
+            {
+                if (targetScale.y > 5)
+                {
+                    transform.localScale = Vector3.one * 5;
+                }
+                if (targetScale.y < 1)
+                {
+                    transform.localScale = Vector3.one;
+                }
+                break;
+            }
+            else
+            {
+                transform.localScale = Vector3.Lerp(targetScale, originalScale, time / originalTime);
+            }
+            yield return null;
+        }
+        isZooming = false;
     }
 }
